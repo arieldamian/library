68ac47a6e877d425b63ff3943e64ac01b754ee74
Ariel Gonzalez <ariel.damian.gonzalez@gmail.com>
2017-04-18 16:10:31 -0300

    Testing EJS engine.

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var port = process.env.PORT || 5000;
 // Setup static routes to files.
 app.use(express.static('public'));
 app.set('views', './src/views');
-app.set('view engine', '.hbs');
+
+app.set('view engine', 'ejs');
 
 var handlebars = require('express-handlebars');
 app.engine('.hbs', handlebars({extname: 'hbs'}));
