5170d9f418631dbfe389dac865aebef312d6d0dc
Ariel Gonzalez <ariel.damian.gonzalez@gmail.com>
2017-04-18 13:46:40 -0300

    First approach to node.

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ var port = process.env.PORT || 5000;
 
 // Setup static routes to files.
 app.use(express.static('public'));
-// app.use(express.static('src/views'));
-app.set('views', './src/views');
-app.set('view engine', 'jade');
+app.use(express.static('src/views'));
+// app.set('views', './src/views');
+// app.set('view engine', 'jade');
 
 app.get('/', function (request, response) {
-    // response.send('Hello world!');
-    response.render('index', {list: ['a', 'b']});
+    response.send('Hello world!');
+    // response.render('index', {list: ['a', 'b']});
 });
 
 app.get('/books', function (request, response) {
