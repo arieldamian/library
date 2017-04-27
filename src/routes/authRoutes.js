e9c0f55473aee43b0b7196184fd83bfe1e48a0d7
Ariel Gonzalez <ariel.damian.gonzalez@gmail.com>
2017-04-27 17:29:05 -0300

    Added signIn and changed the singUp to validate user and redirect.

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,15 +1,35 @@
 var express = require('express');
 var authRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
+var passport = require('passport');
 
 var router = function () {
     authRouter.route('/signUp')
         .post(function (req, res) {
             console.log(req.body);
-            req.login(req.body, function () {
-                res.redirect('/auth/profile');
+            var url = 'mongodb://localhost:27017/libraryApp';
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('users');
+                var user = {
+                    username: req.body.userName,
+                    password: req.body.password
+                };
+
+                collection.insert(user, function (err, results) {
+                    req.login(results.ops[0], function () {
+                        res.redirect('/auth/profile');
+                    });
+                })
             });
         });
+
+    authRouter.route('/signIn')
+        .post(passport.authenticate('local', {
+            failureRedirect: '/'
+        }), function (req, res) {
+            res.redirect('/auth/profile');
+        });
+
     authRouter.route('/profile')
         .get(function (req, res) {
             res.json(req.user);
